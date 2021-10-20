dcc950abb82dcf74e6e3a0c2b90c8b07fc3dc3d6
dev-hjkim <guswlsapdlf@khu.ac.kr>
2021-10-20 23:52:38 +0900

    211020, db 설정 추가

--- auth/auth/src/main/java/com/fashion/celebrity/auth/AuthApplication.java
+++ auth/auth/src/main/java/com/fashion/celebrity/auth/AuthApplication.java
@@ -1,15 +1,35 @@
 package com.fashion.celebrity.auth;
 
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import javax.sql.DataSource;
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@MapperScan(value= {"com.fashion.celebrity.**.mapper"})
 public class AuthApplication {
+	private static final String CLASSPATH_MAPPER_LOCATION = "classpath:/mappers/*.xml";
+	private static final String CLASSPATH_MAPPER_CONFIG_LOCATION = "classpath:/mybatis-config.xml";
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthApplication.class, args);
+	}
+
+	@Bean
+	public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+		SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+		sessionFactory.setDataSource(dataSource);
+		sessionFactory.setMapperLocations(new PathMatchingResourcePatternResolver().getResources(CLASSPATH_MAPPER_LOCATION));
+		sessionFactory.setConfigLocation(new PathMatchingResourcePatternResolver().getResource(CLASSPATH_MAPPER_CONFIG_LOCATION));
+		sessionFactory.getObject().getConfiguration().setMapUnderscoreToCamelCase(true);
+		return sessionFactory.getObject();
 	}
 
 }
